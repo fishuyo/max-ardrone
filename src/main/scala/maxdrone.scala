0ab1b27a63743242fe6a91ca477549acda031cb8
Tim Wood <fishuyo@gmail.com>
2012-09-06 14:00:05 -0700

    connect is now in seperate thread so won't block while waiting, added log4j.properties removes warning, can change logger level with logger message, added more debug parameters to be printed, added reboot message, generally better checking of disconnected state to not cause exceptions or funkiness

--- src/main/scala/maxdrone.scala
+++ src/main/scala/maxdrone.scala
@@ -7,6 +7,7 @@
 */
 
 import com.fishuyo.maths.Vec3
+import com.fishuyo.net.SimpleTelnetClient
 
 import com.codeminders.ardrone._
 
@@ -16,6 +17,8 @@ import MaxObject._
 
 import java.net._
 import java.awt.image.BufferedImage
+
+import org.apache.log4j._
 
 class DroneControl extends MaxObject with NavDataListener with DroneVideoListener {
 
@@ -68,34 +71,103 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
   declareAttribute("vy")
   var vz = 0.f
   declareAttribute("vz")
+
+  var qSize = 0
   
 
-  def connect( ) = {
-    try {
-      drone = new ARDrone( InetAddress.getByName(ip) )
-     	post("connecting to ARDrone at " + ip + " ..." )
-      drone.connect
-      post("ARDrone Connected.")
-      drone.clearEmergencySignal
-      drone.waitForReady(5000)
-      post("ARDrone ready!")
-      drone.trim
-      drone.addImageListener(this)
-      drone.addNavDataListener(this)
-      ready = true
-
-    } catch {
-      case e: Exception => ouch("Drone connection failed..."); e.printStackTrace 
+  def connect(){
+    if( drone != null){
+      post("Drone already connected.")
+      return
     }
+    val control = this
+    val t = new Thread(){
+      override def run(){
+        try {
+          val d = new ARDrone( InetAddress.getByName(ip) )
+         	post("connecting to ARDrone at " + ip + " ..." )
+          d.connect
+          d.clearEmergencySignal
+          d.waitForReady(3000)
+          post("ARDrone connected and ready!")
+          d.trim
+          d.addImageListener(control)
+          d.addNavDataListener(control)
+          drone = d
+          ready = true
+
+        } catch {
+          case e: Exception => ouch("Drone connection failed...")//; e.printStackTrace 
+        }  
+      }
+    }
+    t.start
   }
 
-  def disconnect() = { if( flying ) drone.land; drone.disconnect; Thread.sleep(100); drone = null; post("Drone disconnected.") }
-  def clearEmergency() = drone.clearEmergencySignal
-  def trim() = drone.trim
-  def takeOff() = { drone.takeOff; flying = true}
-  def land() = { drone.land; flying = false}
+  def disconnect(){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    if( flying ) drone.land
+    drone.disconnect
+    Thread.sleep(100)
+    drone = null
+    ready = false
+    post("Drone disconnected.") 
+  }
 
-  def playLed = drone.playLED(1, 10, 5 )
+  def clearEmergency(){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    drone.clearEmergencySignal
+  }
+
+  def trim() = drone.trim
+  
+  def takeOff(){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    drone.takeOff
+  }
+  
+  def land(){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    drone.land
+  }
+
+  def reboot() = {
+    val t = new Thread(){
+      override def run(){
+        try{
+          post("Sending reboot...")
+          if( drone != null ) disconnect
+          val c = new SimpleTelnetClient(ip)
+          c.send("reboot")
+          c.disconnect
+          post("Rebooting ARDrone. ")
+        } catch {
+          case e: Exception => post("Reboot failed.") 
+        }
+      }
+    }
+    t.start
+  }
+
+  def playLed(){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    drone.playLED(1, 10, 5 )
+  }
 
   def toggleFly = {
 	  if( flying ) land
@@ -103,7 +175,14 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
 	  //post( "fly: " + flying )
   }
 
-  def move( lr: Float, fb: Float, ud: Float, r: Float ) = { navigating=false; drone.move(lr,fb,ud,r) }
+  def move( lr: Float, fb: Float, ud: Float, r: Float ){
+    if( drone == null){
+      post("Drone not connected.")
+      return
+    }
+    navigating=false
+    drone.move(lr,fb,ud,r) 
+  }
 
   def moveTo( x:Float,y:Float,z:Float,w:Float ) = {
     dest = Vec3(x,y,z)
@@ -129,8 +208,8 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
     if( dw > 180.f ) dw -= 360.f 
     if( dw < -180.f ) dw += 360.f 
     if( math.abs(dw) > yawThresh ){
-      var r = -rotSpeed //.3f
-      if( dw < 0.f) r = rotSpeed //.3f
+      var r = -rotSpeed
+      if( dw < 0.f) r = rotSpeed
       drone.move(0,0,0,r)
       return
     }
@@ -141,13 +220,13 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
     val cos = math.cos(w.toRadians)
     val sin = math.sin(w.toRadians)
     val d = (dest - pos).normalize
-    var ud = d.y * vmoveSpeed //.1f
+    var ud = d.y * vmoveSpeed
 
     //assumes drone oriented looking down negative z axis, positive x axis to its right
-    var fb = d.x*sin + d.z*cos //-d.x*cos - d.z*sin  //d.x*cos - d.z*sin
-    var lr = d.x*cos - d.z*sin //-d.x*sin + d.z*cos  //-d.x*sin - d.z*cos
-    fb = fb * moveSpeed //.1f
-    lr = lr * moveSpeed //.1f
+    var fb = d.x*sin + d.z*cos 
+    var lr = d.x*cos - d.z*sin
+    fb = fb * moveSpeed
+    lr = lr * moveSpeed
     //println("dp: " + dp + "  "+lr+" "+fb+" "+ud)
     if( dp  > posThresh ){
       drone.move(lr.toFloat,fb.toFloat,ud,0)
@@ -157,13 +236,28 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
     //println( dp )
   }
 
+  def logger( l:String="INFO" ){
+    var v = Level.INFO
+    l.toUpperCase match {
+      case "WARN" => v = Level.WARN
+      case "DEBUG" => v = Level.DEBUG
+      case _ => v = Level.INFO
+    }
+    Logger.getRootLogger.setLevel( v )
+  }
+
   def debug(){
     post( "ready: " + ready)
     post("flying: " + flying)
     post("navigating: " + navigating)
     post("pos: " + pos.x + " " + pos.y + " " + pos.z + " " + yaw)
     post("dest: " + dest.x + " " + dest.y + " " + dest.z + " " + destYaw)
-
+    post("tracked vel: " + vel.x + " " + vel.y + " " + vel.z)
+    post("internal vel: " + vx + " " + vy + " " + vz)
+    post("altitude: " + altitude)
+    post("pitch roll yaw: " + pitch + " " + roll + " " + yaww)
+    post("battery: " + battery)
+    post("command queue size: " + qSize)
   }
 
   def navDataReceived(nd:NavData){
@@ -176,6 +270,7 @@ class DroneControl extends MaxObject with NavDataListener with DroneVideoListene
     vx = nd.getVx
     vy = nd.getLongitude
     vz = nd.getVz
+    qSize = drone.queueSize
   }
 
   def frameReceived(startX:Int, startY:Int, w:Int, h:Int, rgbArray:Array[Int], offset:Int, scansize:Int){
